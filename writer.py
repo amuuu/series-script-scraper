6b7eea92af47540d3aa295856996b5f812cfa55c
Amirreza Amouie <aamouie01@gmail.com>
2018-09-30 22:37:13 +0330

    correct writer.py

--- writer.py
+++ writer.py
@@ -1,7 +1,8 @@
 import scrape
 
+
 def write_script(name, season_no, episode_number, script, directory='./'):
-    file_name = directory + scrape.name + "s" + season_no + "e" + episode_number
+    file_name = directory + name + "s" + scrape.number_to_str(season_no) + "e" + scrape.number_to_str(episode_number)
     f = open(file_name, 'w')
     f.write(script)
     return True
