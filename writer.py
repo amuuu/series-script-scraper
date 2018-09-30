7355342481afad7e6ea29a6152b452282b04d860
Amirreza Amouie <aamouie01@gmail.com>
2018-09-30 23:12:19 +0330

    encode files with utf-8

--- writer.py
+++ writer.py
@@ -5,6 +5,6 @@ def write_script(name, season_no, episode_number, script, directory):
     # if directory[len(directory)-1]!= '\\':
         # pass
     file_name = directory + name + "-s" + scrape.number_to_str(season_no) + "e" + scrape.number_to_str(episode_number) + ".txt"
-    f = open(file_name, 'w')
+    f = open(file_name, 'w', encoding="utf-8")
     f.write(script)
     return True
