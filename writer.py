a56d42656cf8785c143758d8dac5df8a02267c4c
Amirreza Amouie <aamouie01@gmail.com>
2018-09-30 22:38:24 +0330

    save file as .txt

--- writer.py
+++ writer.py
@@ -2,7 +2,7 @@ import scrape
 
 
 def write_script(name, season_no, episode_number, script, directory='./'):
-    file_name = directory + name + "s" + scrape.number_to_str(season_no) + "e" + scrape.number_to_str(episode_number)
+    file_name = directory + name + "s" + scrape.number_to_str(season_no) + "e" + scrape.number_to_str(episode_number) + ".txt"
     f = open(file_name, 'w')
     f.write(script)
     return True
