53bec8d90061f9b11ad7629b7b259e80f1a09366
Amirreza Amouie <aamouie01@gmail.com>
2018-09-30 22:58:59 +0330

    change files name

--- writer.py
+++ writer.py
@@ -4,7 +4,7 @@ import scrape
 def write_script(name, season_no, episode_number, script, directory):
     # if directory[len(directory)-1]!= '\\':
         # pass
-    file_name = directory + name + "s" + scrape.number_to_str(season_no) + "e" + scrape.number_to_str(episode_number) + ".txt"
+    file_name = directory + name + "-s" + scrape.number_to_str(season_no) + "e" + scrape.number_to_str(episode_number) + ".txt"
     f = open(file_name, 'w')
     f.write(script)
     return True
